7e4fd48ca55f71de23366a69513ec8b11f526d95
Shivani <lshivanil1985@gmail.com>
2021-01-18 11:50:46 +0530

    add remote database

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -24,12 +24,12 @@ const env_var = {
         },
         port: process.env.PORT || 3100,
     },
+
     localTesting: {
-        HOST: "localhost",
-        USER: "root",
-        PASSWORD: "lanetteam@1",
-        DB: "community_db",
-        // firebaseAdmin: admin,
+        HOST: "us-cdbr-east-03.cleardb.com",
+        USER: "bded972b8d7398",
+        PASSWORD: "f29dff2d",
+        DB: "heroku_0f888c1c6017d15",
         dialect: "mysql",
         pool: {
             max: 5,
