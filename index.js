cfe89a3e91284dbe39c5b4b8260b79bb44ecc35c
Shivani <lshivanil1985@gmail.com>
2021-01-18 12:25:00 +0530

    add start script

--- index.js
+++ index.js
@@ -56,6 +56,16 @@ app.use(bodyParser.json());
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.get("/", (req, res) => res.send("Hello World!"));
 app.use("/userAuthentication", userAuthentication);
 app.use("/userActions", userActions);
