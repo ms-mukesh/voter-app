dd6d445d6d93f0428b6d178af0f1079f5167c882
Shivani <lshivanil1985@gmail.com>
2021-01-19 18:29:37 +0530

    final commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,4 +1,4 @@
-const cenv = "liveClientTesting";
+const cenv = "localTesting";
 // const admin = require("firebase-admin");
 // eslint-disable-next-line import/no-unresolved
 // const serviceAccount = require("../firebase/navgam-production-firebase-adminsdk-rifa7-86a0dd388c.json");
