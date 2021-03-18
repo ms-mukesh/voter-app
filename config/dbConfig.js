127b985c49473f9090f6d2d95bc16b8f20b2dcfa
Shivani <lshivanil1985@gmail.com>
2021-03-18 13:05:37 +0530

    final commit before moving to new server

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 const cenv = "localTesting";
 const env_var = {
     localTesting: {
-        HOST: "localhost",
+        HOST: "139.59.1.188",
         USER: "root_admin",
         PASSWORD: "Abcdef123!@#",
         DB: "community_db",
