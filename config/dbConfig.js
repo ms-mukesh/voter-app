8a56e28fdd4fb5dc78f6748eef747567d4418e99
Shivani <lshivanil1985@gmail.com>
2021-04-17 09:25:41 +0530

    change db config from didigtal ocean on EC2

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -16,10 +16,26 @@ const env_var = {
 
         port: process.env.PORT || 3100,
     },
+    // remoteTesting: {
+    //     HOST: "139.59.1.188",
+    //     USER: "root_admin",
+    //     PASSWORD: "Abcdef123!@#",
+    //     DB: "community_db",
+    //     // firebaseAdmin: admin,
+    //     dialect: "mysql",
+    //     pool: {
+    //         max: 5,
+    //         min: 0,
+    //         acquire: 30000,
+    //         idle: 10000,
+    //     },
+    //
+    //     port: process.env.PORT || 3100,
+    // },
     remoteTesting: {
-        HOST: "139.59.1.188",
-        USER: "root_admin",
-        PASSWORD: "Abcdef123!@#",
+        HOST: "3.14.151.80",
+        USER: "mukesh_local",
+        PASSWORD: "mukesh2103",
         DB: "community_db",
         // firebaseAdmin: admin,
         dialect: "mysql",
